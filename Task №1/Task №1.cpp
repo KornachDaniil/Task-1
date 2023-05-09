05211093694945f2663d522b74438a4534b69c03
Daniil Kornach <danik.kornach@gmail.com>
2023-05-09 18:16:57 +0300

    Удалил лишние переменные.

--- Task №1/Task №1.cpp
+++ Task №1/Task №1.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	char arr[80] = "I love you";
-	int number = 5;
-	int count = 0;
 	char String[] = "Hello 2023 year. ~3256Ёё123";
+	int count = 0;
 	for (int i = 0; String[i]; i++) {
 		if ((int(String[i]) & 1) == 0) {
 			cout << (int)String[i] << ' ';
