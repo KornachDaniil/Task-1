25a7311a0a6ea240e078de30320cb46f8e49229e
Daniil Kornach <danik.kornach@gmail.com>
2023-05-09 18:11:51 +0300

    Добавьте файлы проекта.

---
+++ Task №1/Task №1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	char arr[80] = "I love you";
+	int number = 5;
+	int count = 0;
+	char String[] = "Hello 2023 year. ~3256Ёё123";
+	for (int i = 0; String[i]; i++) {
+		if ((int(String[i]) & 1) == 0) {
+			cout << (int)String[i] << ' ';
+			count++;
+		}
+	}
+	cout << "Количество четных: " << count << endl;
+	return 0;
+}
