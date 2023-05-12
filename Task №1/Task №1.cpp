0948a0dc09d021b36503cd37615d6a1f75cdb46b
Daniil Kornach <danik.kornach@gmail.com>
2023-05-12 22:12:16 +0300

    Поправил ошибки.

--- Task №1/Task №1.cpp
+++ Task №1/Task №1.cpp
@@ -6,7 +6,7 @@ int main() {
 	char String[] = "Hello 2023 year. ~3256Ёё123";
 	int count = 0;
 	for (int i = 0; String[i]; i++) {
-		if ((int(String[i]) & 1) == 0) {
+		if (((int)String[i] & 1) == 0) {
 			cout << (int)String[i] << ' ';
 			count++;
 		}
