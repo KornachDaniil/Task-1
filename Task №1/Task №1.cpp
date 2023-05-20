d43f815a4982a44f6b1a3ec7c5e31b4e51988bea
Daniil Kornach <danik.kornach@gmail.com>
2023-05-20 15:58:54 +0300

    Поправил 9 строку.

--- Task №1/Task №1.cpp
+++ Task №1/Task №1.cpp
@@ -6,7 +6,7 @@ int main() {
     char string[150] = "Hello 2023 year. ~3256Ёё123";
     int count = 0;
 
-    for (int i = 0; i < strlen(string); i++) {
+    for (int i = 0; string[i]; i++) {
         if (isdigit(string[i]) != 0) {
             if (((string[i] - '0') & 1) == 0) {
                 count++;
