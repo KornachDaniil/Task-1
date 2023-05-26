3a3d04a60a6393c632c00d40d4b3b5d64adc40ef
Daniil Kornach <danik.kornach@gmail.com>
2023-05-26 10:37:30 +0300

    Сделал локальным указатель "pstring" т.к после выполнения цикла "for" он не используется.

--- Task №1/Task №1.cpp
+++ Task №1/Task №1.cpp
@@ -1,15 +1,15 @@
 ﻿#include <iostream>
+#include <cctype>
 
 int main() {
     using namespace std;
     setlocale(0, "rus");
-    char string[150] = "Hello 2023 year. ~3256Ёё123";
-    char* pstring;
+    char string[] = "Hello 2023 year. ~3256Ёё123";
     int count = 0;
-    pstring = string;
+    
 
-    for (int i = 0; *(pstring + i); i++) {
-        if (isdigit(*(pstring + i)) && (*(pstring + i) & 1) == 0) {
+    for (char* pstring = string; *(pstring++); ) {
+        if (isdigit(*pstring) && (*pstring & 1) == 0) {
             count++;
         }
     }
